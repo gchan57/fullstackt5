c2ad2c67fc6501dc319e795fd8b23390f4d78ee7
Gunachandru <tgunachandru@gmail.com>
2025-12-08 18:35:58 +0530

    html,css completed

---
+++ calculator/script.js
@@ -0,0 +1,14 @@
+const display=document.querySelector('.display');
+const buttons=document.querySelector('.btn');
+let currentExpression='';
+
+buttons.forEach(button=>{
+    button.addEventListener('click',()=>{
+        if(value === '='){
+
+        }
+        else{
+            
+        }
+    })
+})
