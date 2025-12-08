de63e0bbfb2cb7a7e7975273b47ff02b0f6d7895
Gunachandru <tgunachandru@gmail.com>
2025-12-08 19:00:19 +0530

    completed

--- calculator/script.js
+++ calculator/script.js
@@ -1,14 +1,28 @@
 const display=document.querySelector('.display');
-const buttons=document.querySelector('.btn');
+const buttons = document.querySelectorAll('.btn');
 let currentExpression='';
 
 buttons.forEach(button=>{
     button.addEventListener('click',()=>{
-        if(value === '='){
-
+        const value = button.textContent;
+        if(value === 'clear'){
+         currentExpression ='';
+            display.value=currentExpression;  
+        }
+        else if(value === '='){
+            try{
+                const result=eval(currentExpression);
+                display.value=result;
+                currentExpression=String(result);
+            }
+            catch{
+                display.value='Error';
+                currentExpression='';
+            }
         }
         else{
-            
+            currentExpression+=value;
+            display.value=currentExpression;
         }
-    })
-})
+    });
+});
