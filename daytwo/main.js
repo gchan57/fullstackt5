d258ff516ac1361a561414e3026049fc2ba6e5ff
Gunachandru <tgunachandru@gmail.com>
2025-12-09 10:44:17 +0530

    feat: added folder logic

--- daytwo/main.js
+++ daytwo/main.js
@@ -2,3 +2,11 @@ const {add,mul,sub}=require("./app.js")
 console.log(add(700,5))
 console.log(mul(9,5))
 console.log(sub(34,5))
+const fs= require("fs");
+fs.writeFile("hello.txt","Hello World","UTF-8",(error)=>{
+    if(error){
+        console.log(error);
+        return;
+    }
+    console.log("created successfully");
+})
